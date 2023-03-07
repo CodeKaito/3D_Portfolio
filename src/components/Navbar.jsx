6ae4406b5cae95d572c69d5142739a86f9aab63f
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-07 19:56:34 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { logo2, menu, close } from '../assets';
 
 const Navbar = () => {
   return (
@@ -12,9 +12,10 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
         <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
           <Link to="/" className='flex items-center gap-2' onClick={() => { setActive(""); window.scrollTo(0, 0);}}>
-            <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+            <img src={logo2} alt="logo2" className='w-12 h-12 object-contain' />
             <p className='text-white text-[18px] font-bold cursor-pointer'> Jerome <span className='sm:block hidden'>| Personal Portfolio</span></p>
           </Link>
+          <p className='text-red-500'>asdsa</p>
         </div>
     </nav>
   )
