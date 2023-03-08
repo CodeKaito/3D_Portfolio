3219ef1fe440a03729a142be30ef0825e54fd86b
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-08 19:08:40 +0100

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi there, I'm <span className='text-[#915EFF]'>Jerome</span>
+            Hi there, I'm <span className='text-[#ffffff]'>Jerome</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I'm a passionate web developer,  <br className='sm:block hidden' />
