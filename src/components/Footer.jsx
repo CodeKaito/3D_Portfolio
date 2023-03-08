bc6b2a2e94b1c36508d436d836781ff24f481fbb
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-08 15:02:06 +0100

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,10 +14,18 @@ const Footer = () => {
             <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5'>Kaito Inc.</h1>
 
             <div className='cursor-pointer space-between justify-start justify-between flex md:w-[10%] my-6'>
-                <FaFacebookSquare size={30} />
-                <FaInstagram size={30} />
-                <FaTwitterSquare size={30} />
-                <FaGithubSquare size={30} />
+                <a href='https://www.facebook.com/jeromeecarl'>
+                    <FaFacebookSquare size={30} />
+                </a>
+                <a href='https://www.instagram.com/carljeromee/'>
+                    <FaInstagram size={30} />
+                </a>
+                <a href='https://twitter.com/carljeromeoff'>
+                    <FaTwitterSquare size={30} />
+                </a>
+                <a href='https://github.com/CodeKaito'>
+                    <FaGithubSquare size={30} />
+                </a>
             </div>
             <span className='footer__copy'>
                 &#169; 2023 | CodeKaito. All right reserved
