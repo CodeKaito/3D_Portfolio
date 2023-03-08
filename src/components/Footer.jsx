cea12eb1d857c45f96241bae07c3bc6fa133a284
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-08 14:57:23 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import {
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare,
+  } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <footer className='bg-white-700 text-black'>
         <div className='md:flex md:justify-between md:items-center sm:px-12 px-4 bg-[#fffffff] py-7'>
             <h1 className='lg:text-4xl text-3xl md:mb-0 mb-6 lg:leading-normal font-semibold md:w-2/5'>Kaito Inc.</h1>
 
+            <div className='cursor-pointer justify-start justify-between flex md:w-[10%] my-6'>
+                <FaFacebookSquare size={30} />
+                <FaInstagram size={30} />
+                <FaTwitterSquare size={30} />
+                <FaGithubSquare size={30} />
+            </div>
             <span className='footer__copy'>
                 &#169; 2023 | CodeKaito. All right reserved
             </span>
